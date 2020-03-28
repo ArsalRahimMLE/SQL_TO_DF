5a0c73ea069403816e5e7a1e64634f834c25df8e
sehan10 <sehan_ahmed@live.com>
2020-03-28 18:18:33 +0500

    completed to_sql and read_sql

--- bcpandas/main.py
+++ bcpandas/main.py
@@ -1,6 +1,6 @@
 # Imports #
 import sqlalchemy
-import pyodbc
+import os, fnmatch
 import pandas as pd
 
 class sqlCred:
@@ -9,19 +9,33 @@ class sqlCred:
                 host: str = '127.0.0.1',
                 username : str = 'SA',
                 password :str = 'abc$12345',
-                driver: str = '/opt/microsoft/msodbcsql17/lib64/libmsodbcsql-17.5.so.2.1'
+                OS:str='Linux',
+
                 ):
+        """
+        :param database: Specify the name of the database from which you want to fetch the data
+        :param host (optional): By default it is localhost (127.0.0.1) but in case of remote database you can replace the IP of the server #
+        :param username: Database user name
+        :param password: Password for database username
+        :param OS (Optional): By default it is 'linux' but in case of windows specify 'Windows'
+        """
+        # Set values to local variables #
         if not database:
             pass # Need to add raise exception later #
         self.database = database
         if username and password:
             self.username = username
             self.password = password
-        self.driver = driver
-        # create a connection string #
-        self.conn = 'mssql+pyodbc://'+username+':'+password+'@'+host+'/'+database+'?driver='+driver
-        #self.conn = 'mssql+pyodbc://SA:abc$12345@127.0.0.1/Northwind_Db?driver=/opt/microsoft/msodbcsql17/lib64/libmsodbcsql-17.5.so.2.1'
-        print('hello')
+        self.host = host
+        # if OS is linux #
+        if OS == 'Linux':
+            self.driver = find_driver_linux('libmsodbcsql*', '/opt/')
+            # create a connection string #
+            self.conn = 'mssql+pyodbc://'+self.username+':'+self.password+'@'+self.host+'/'+self.database+'?driver='+self.driver
+        # else windows #
+        else:
+            self.conn = 'mssql+pyodbc://' + self.username + ':' + self.password + '@' + self.host + '/' + self.database + '?driver=' + self.driver
+
 
 def establish_connection(connection_string):
     # 01 create connection using sqlalchemy #
@@ -38,8 +52,26 @@ def read_sql(credentials:sqlCred, table_name:str):
     connection = establish_connection(conn)
     # 03 fetch the data from the database #
     dt = pd.read_sql_query('SELECT * FROM '+table_name, connection)
-    # 04 return the dataframe #
+    # 04 close the connection #
+    #connection.close()
+    # 05 return the dataframe #
     return dt
 
-def to_sql():
-    print('to sql method')
+
+def to_sql(credentials:sqlCred, dataframe:pd.DataFrame, table_name:str):
+    # 01 fetch the connection string from the sqlCred class #
+    conn = credentials.conn
+    # 02 establish connection using connection string #
+    connection = establish_connection(conn)
+    # 03 store data to the database #
+    dataframe.to_sql(name=table_name, con=connection)
+    # 04 close the connection #
+    #connection.close()
+
+def find_driver_linux(pattern, path):
+    result = []
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            if fnmatch.fnmatch(name, pattern):
+                result.append(os.path.join(root, name))
+    return result[0]
--- testing.py
+++ testing.py
@@ -3,5 +3,5 @@ from bcpandas import sqlCred, read_sql, to_sql
 if __name__=="__main__":
     creds = sqlCred(database='Northwind_Db',username='SA',password='abc$12345')
     dt = read_sql(creds, table_name='Customers')
-    print(len(dt))
-  
+    to_sql(creds,dt,table_name='testing')
+    print(dt.iloc[2])
